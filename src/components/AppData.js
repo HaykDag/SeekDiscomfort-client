0628e63f413928138da86ebb279696cbf347e6d7
HaykDag <106971157+HaykDag@users.noreply.github.com>
2023-09-20 10:26:36 +0800

    Update AppData.js Base-Url from localhost to vercel api

--- src/components/AppData.js
+++ src/components/AppData.js
@@ -1,5 +1,5 @@
-
-const BASE_URL = "http://localhost:4000/"
+const BASE_URL="https://seek-discomfort-rfy3jgcs5-haykdag.vercel.app/"
+//const BASE_URL = "http://localhost:4000/"
 export const AppUrl = {
     Categories: BASE_URL + "categories/",
     Items: BASE_URL + "items/",
