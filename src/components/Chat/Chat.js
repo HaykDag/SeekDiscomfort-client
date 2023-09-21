819a96198be19a0106131f92b9776e915fad123d
HaykDag <haykdaghl@gmail.com>
2023-09-21 09:13:08 +0800

    implimenting chat

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,89 +17,87 @@ import { AppUrl } from "../AppData";
 const socket = io.connect(AppUrl.Base);
 
 const Chat = () => {
-    // const [isChatOpen, setIsChatOpen] = useState(false);
-    // const { user } = useSelector(selectUser);
+    const [isChatOpen, setIsChatOpen] = useState(false);
+    const { user } = useSelector(selectUser);
 
-    // const [messageList, setMessageList] = useState([
-    //     {
-    //         message:
-    //             "Hello Welcom to our support chat, please fell free to ask anything.",
-    //         sentTime: "don't know",
-    //         sender: "Admin",
-    //         direction: "incoming",
-    //     },
-    // ]);
+    const [messageList, setMessageList] = useState([
+        {
+            message:
+                "Hello Welcom to our support chat, please fell free to ask anything.",
+            sentTime: "don't know",
+            sender: "Admin",
+            direction: "incoming",
+        },
+    ]);
 
-    // useEffect(() => {
-    //     socket?.on("receive_message", (message) => {
-    //         const data = {
-    //             message,
-    //             sentTime: `${new Date().getHours()}:${new Date().getMinutes()}`,
-    //             sender: "Admin",
-    //             direction: "incoming",
-    //         };
+    useEffect(() => {
+        socket?.on("receive_message", (message) => {
+            const data = {
+                message,
+                sentTime: `${new Date().getHours()}:${new Date().getMinutes()}`,
+                sender: "Admin",
+                direction: "incoming",
+            };
 
-    //         setMessageList((currentList) => {
-    //             return [...currentList, data];
-    //         });
-    //     });
-    // }, []);
+            setMessageList((currentList) => {
+                return [...currentList, data];
+            });
+        });
+    }, []);
 
-    // const handleSend = (messageText) => {
-    //     const data = {
-    //         message: messageText,
-    //         sentTime: `${new Date().getHours()}:${new Date().getMinutes()}`,
-    //         sender: user ? user.username : "User",
-    //         user_id: user?.id,
-    //         direction: "outgoing",
-    //     };
-    //     socket.emit("send_message", { ...data, id: socket.id });
-    //     setMessageList((currMessList) => {
-    //         return [...currMessList, data];
-    //     });
-    // };
+    const handleSend = (messageText) => {
+        const data = {
+            message: messageText,
+            sentTime: `${new Date().getHours()}:${new Date().getMinutes()}`,
+            sender: user ? user.username : "User",
+            user_id: user?.id,
+            direction: "outgoing",
+        };
+        socket.emit("send_message", { ...data, id: socket.id });
+        setMessageList((currMessList) => {
+            return [...currMessList, data];
+        });
+    };
 
-    // return (
-    //     <>
-    //         {!isChatOpen && (
-    //             <div className="open-chat">
-    //                 <div className="open-chat-inner"></div>
-    //                 <button className="open-chat-btn" onClick={() => setIsChatOpen(!isChatOpen)}>
-    //                     Chat
-    //                 </button>
-    //             </div>
-    //         )}
-    //         {isChatOpen && (
-    //             <div className="chat-cnt" >
-    //                 <MainContainer>
-    //                     <ChatContainer>
-    //                         <ConversationHeader
-    //                             className="chat-header"
-    //                             onClick={() => setIsChatOpen(!isChatOpen)}
-    //                         >
-    //                             <ConversationHeader.Content
-    //                                 className="chat-header-content"
-    //                                 userName="Admin"
-    //                             />
-    //                         </ConversationHeader>
-    //                         <MessageList>
-    //                             {messageList?.map((data, i) => (
-    //                                 <Message key={i} model={data}></Message>
-    //                             ))}
-    //                         </MessageList>
-    //                         <MessageInput
-    //                             onSend={handleSend}
-    //                             placeholder="Type message here"
-    //                         />
-    //                     </ChatContainer>
-    //                 </MainContainer>
-    //             </div>
-    //         )}
-    //     </>
-    // );
     return (
-        <div>chat</div>
-    )
+        <>
+            {!isChatOpen && (
+                <div className="open-chat">
+                    <div className="open-chat-inner"></div>
+                    <button className="open-chat-btn" onClick={() => setIsChatOpen(!isChatOpen)}>
+                        Chat
+                    </button>
+                </div>
+            )}
+            {isChatOpen && (
+                <div className="chat-cnt" >
+                    <MainContainer>
+                        <ChatContainer>
+                            <ConversationHeader
+                                className="chat-header"
+                                onClick={() => setIsChatOpen(!isChatOpen)}
+                            >
+                                <ConversationHeader.Content
+                                    className="chat-header-content"
+                                    userName="Admin"
+                                />
+                            </ConversationHeader>
+                            <MessageList>
+                                {messageList?.map((data, i) => (
+                                    <Message key={i} model={data}></Message>
+                                ))}
+                            </MessageList>
+                            <MessageInput
+                                onSend={handleSend}
+                                placeholder="Type message here"
+                            />
+                        </ChatContainer>
+                    </MainContainer>
+                </div>
+            )}
+        </>
+    );
+   
 };
 
 export default Chat;
--- src/pages/AdminChat/AdminChat.js
+++ src/pages/AdminChat/AdminChat.js
@@ -1,114 +1,111 @@
-// import {
-//     ConversationList,
-//     MainContainer,
-//     ChatContainer,
-//     MessageList,
-//     Message,
-//     MessageInput,
-//     Conversation,
-//     Sidebar,
-//     ConversationHeader,
-// } from "@chatscope/chat-ui-kit-react";
-// import "./adminChat.css";
-// import io from "socket.io-client";
-// import { useEffect, useState } from "react";
-
-//const socket = io.connect("http://localhost:4000");
+import {
+    ConversationList,
+    MainContainer,
+    ChatContainer,
+    MessageList,
+    Message,
+    MessageInput,
+    Conversation,
+    Sidebar,
+    ConversationHeader,
+} from "@chatscope/chat-ui-kit-react";
+import "./adminChat.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+import { AppUrl } from "../../components/AppData";
+const socket = io.connect(AppUrl.Base);
 
 const AdminChat = () => {
-    // const [currUser, setCurrUser] = useState({});
-    // const [users, setUsers] = useState({});
-    // const [messageList, setMessageList] = useState([]);
+    const [currUser, setCurrUser] = useState({});
+    const [users, setUsers] = useState({});
+    const [messageList, setMessageList] = useState([]);
 
-    // const filteredMessages = messageList.filter((message) => {
-    //     return message.id === currUser.id;
-    // });
+    const filteredMessages = messageList.filter((message) => {
+        return message.id === currUser.id;
+    });
 
-    // useEffect(() => {
-    //     socket?.emit("register");
-    //     socket?.on("receive_message", (data) => {
-    //         setMessageList((currList) => {
-    //             return [...currList, { ...data, direction: "incoming" }];
-    //         });
-    //         if (!users.hasOwnProperty(data.id)) {
-    //             users[data.id] = {
-    //                 name: data.sender,
-    //                 user_id: data.user_id,
-    //                 unread: currUser.id !== data.id,
-    //                 active: true,
-    //             };
-    //             setUsers((u) => u);
-    //         } else {
-    //             users[data.id].unread = currUser.id !== data.id;
-    //         }
-    //     });
-    //     socket.on("user_disconnected", (id) => {
-    //         users[id].active = false;
-    //         setUsers((u) => u);
-    //     });
-    // }, []);
+    useEffect(() => {
+        socket?.emit("register");
+        socket?.on("receive_message", (data) => {
+            setMessageList((currList) => {
+                return [...currList, { ...data, direction: "incoming" }];
+            });
+            if (!users.hasOwnProperty(data.id)) {
+                users[data.id] = {
+                    name: data.sender,
+                    user_id: data.user_id,
+                    unread: currUser.id !== data.id,
+                    active: true,
+                };
+                setUsers((u) => u);
+            } else {
+                users[data.id].unread = currUser.id !== data.id;
+            }
+        });
+        socket.on("user_disconnected", (id) => {
+            users[id].active = false;
+            setUsers((u) => u);
+        });
+    }, []);
 
-    // const handleReply = (text) => {
-    //     if (!socket) return;
-    //     const data = {
-    //         message: text,
-    //         name: currUser.name,
-    //         id: currUser.id,
-    //         direction: "outgoing",
-    //     };
+    const handleReply = (text) => {
+        if (!socket) return;
+        const data = {
+            message: text,
+            name: currUser.name,
+            id: currUser.id,
+            direction: "outgoing",
+        };
 
-    //     setMessageList((currentList) => {
-    //         return [...currentList, data];
-    //     });
-    //     socket.emit("reply", data);
-    // };
-    // const changeConversation = (id, name) => {
-    //     setCurrUser({ id, name });
-    //     users[id].unread = false;
-    // };
-    // return (
-    //     <div className="Admin-chat-cnt">
-    //         <MainContainer className="chat-main-cnt">
-    //             <Sidebar position="left" scrollable={true}>
-    //                 <ConversationList>
-    //                     <div className="con-list-header">Conversations</div>
-    //                     {Object.entries(users).map((entry) => (
-    //                         <Conversation
-    //                             onClick={() =>
-    //                                 changeConversation(entry[0], entry[1].name)
-    //                             }
-    //                             key={entry[0]}
-    //                             name={entry[1].name}
-    //                             info={`User id: ${entry[1].user_id}`}
-    //                             unreadDot={entry[1].unread}
-    //                             active={entry[0] === currUser.id}
-    //                         ></Conversation>
-    //                     ))}
-    //                 </ConversationList>
-    //             </Sidebar>
-    //             <ChatContainer>
-    //                 <ConversationHeader>
-    //                     <ConversationHeader.Content
-    //                         className="chat-header"
-    //                         userName={currUser.name}
-    //                     />
-    //                 </ConversationHeader>
-    //                 <MessageList>
-    //                     {filteredMessages.map((data, i) => (
-    //                         <Message key={i} model={data}></Message>
-    //                     ))}
-    //                 </MessageList>
-    //                 <MessageInput
-    //                     onSend={handleReply}
-    //                     placeholder="type your message here"
-    //                 />
-    //             </ChatContainer>
-    //         </MainContainer>
-    //     </div>
-    // );
+        setMessageList((currentList) => {
+            return [...currentList, data];
+        });
+        socket.emit("reply", data);
+    };
+    const changeConversation = (id, name) => {
+        setCurrUser({ id, name });
+        users[id].unread = false;
+    };
     return (
-        <div>Admin Chat</div>
-    )
+        <div className="Admin-chat-cnt">
+            <MainContainer className="chat-main-cnt">
+                <Sidebar position="left" scrollable={true}>
+                    <ConversationList>
+                        <div className="con-list-header">Conversations</div>
+                        {Object.entries(users).map((entry) => (
+                            <Conversation
+                                onClick={() =>
+                                    changeConversation(entry[0], entry[1].name)
+                                }
+                                key={entry[0]}
+                                name={entry[1].name}
+                                info={`User id: ${entry[1].user_id}`}
+                                unreadDot={entry[1].unread}
+                                active={entry[0] === currUser.id}
+                            ></Conversation>
+                        ))}
+                    </ConversationList>
+                </Sidebar>
+                <ChatContainer>
+                    <ConversationHeader>
+                        <ConversationHeader.Content
+                            className="chat-header"
+                            userName={currUser.name}
+                        />
+                    </ConversationHeader>
+                    <MessageList>
+                        {filteredMessages.map((data, i) => (
+                            <Message key={i} model={data}></Message>
+                        ))}
+                    </MessageList>
+                    <MessageInput
+                        onSend={handleReply}
+                        placeholder="type your message here"
+                    />
+                </ChatContainer>
+            </MainContainer>
+        </div>
+    );
 };
 
 export default AdminChat;
